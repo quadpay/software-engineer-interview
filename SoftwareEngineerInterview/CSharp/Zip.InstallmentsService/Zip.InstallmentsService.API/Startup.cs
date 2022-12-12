168ab3c8c5188c8453ee7dff063e4eb1954b80c3
satish hirpara <satish.hirpara09@gmail.com>
2022-12-12 11:05:41 +0530

    JWT Token Implementation

--- SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService.API/Startup.cs
+++ SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService.API/Startup.cs
@@ -19,6 +19,9 @@ using Zip.InstallmentsService.Data.Repository;
 using Zip.InstallmentsService.Entity.Dto;
 using Zip.InstallmentsService.Core.Implementation;
 using Zip.InstallmentsService.Core.Interface;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Zip.InstallmentsService.Service
 {
@@ -59,6 +62,33 @@ namespace Zip.InstallmentsService.Service
             //    config.AddDebug();
             //    config.AddConsole();
             //});
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettingDto>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettingDto>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             services.AddApplicationInsightsTelemetry();
 
---
+++ SoftwareEngineerInterview/CSharp/Zip.InstallmentsService/Zip.InstallmentsService.Entity/Dto/AppSettingDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zip.InstallmentsService.Entity.Dto
+{
+    public class AppSettingDto
+    {
+        public string Secret { get; set; }
+    }
+}
